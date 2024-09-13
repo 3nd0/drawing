6acb5378080311d05f10d8f3147effdf84078c4e
wataru endo <w4.3nd0@watarunoMacBook-Air.local>
2024-09-13 17:02:34 +0900

    Initialize

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,3 @@
-import { generate, count } from "random-words";
-
 export default function Home() {
-  const word = generate()
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {word}
-    </main>
-  );
+    return <></>
 }
